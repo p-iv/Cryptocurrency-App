a645abb5d110378533554b1296f448a7b0626a61
p-iv <pavlo.00@gmail.com>
2024-03-04 10:32:59 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import axios from "axios";
 import Coins from "./compnents/Coins";
 import Search from "./compnents/Search";
 import "./App.css";
+import Loader from "./compnents/Loader";
 
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(
     function () {
+      setIsLoading(true);
       axios
         .get(
           "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=50&x_cg_demo_api_key=CG-QYNjgC24hDYWX86bkmGzkavr"
         )
         .then((res) => {
+          setIsLoading(false);
           setCoins(res.data);
         })
         .catch((error) => {
@@ -32,7 +36,7 @@ function App() {
   return (
     <div className="crypto-app">
       <Search setSearch={setSearch} />
-      <Coins filteredCoins={filteredCoins} />
+      {isLoading ? <Loader /> : <Coins filteredCoins={filteredCoins} />}
     </div>
   );
 }
